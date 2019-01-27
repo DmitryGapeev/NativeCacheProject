dc917857e59411fb9b6b31608e295c3cc1217de2
Дмитрий Гапеев <gapeevdirog@gmail.com>
2019-01-27 19:39:35 +0300

    Add project files.

---
+++ NativeCacheProject/NativeCache.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Security.Policy;
+using System.Text;
+
+namespace AlgorithmsDataStructures
+{
+
+  public class NativeCache<T>
+  {
+    public int size;
+    public string[] slots;
+    public T[] values;
+    public int[] hits;
+
+    private int step = 3;
+
+    public NativeCache(int sz)
+    {
+      size = sz < 10  ? 10 : sz;
+      slots = new string[size];
+      values = new T[size];
+      hits = new int[size];
+    }
+
+    public int HashFun(string key)
+    {
+      if (key == null)
+        return -1;
+
+      int sumBytes = 0;
+      byte[] valuesBytes = Encoding.UTF8.GetBytes(key);
+
+      foreach (byte item in valuesBytes)
+        sumBytes += item;
+
+      return sumBytes % slots.Length;
+    }
+
+    private int SeekPosition(string key)
+    {
+      int position = HashFun(key);
+      for (int i = 0; i < slots.Length; i++)
+      {
+        if (slots[position] == null || slots[position] == key)
+          return position;
+
+        position += step;
+        position %= slots.Length;
+      }
+
+      return -1;
+    }
+
+    public bool IsExist(string key)
+    {
+      if (key == null)
+        return false;
+
+      return GetKeyPosition(key) > 0;
+    }
+
+    private int GetKeyPosition(string key)
+    {
+      int keyPosition = HashFun(key);
+
+      for (int i = 0; i < slots.Length; i++)
+      {
+        if (slots[keyPosition] == null)
+          return -1;
+        if (slots[keyPosition] == key)
+          return keyPosition;
+
+        keyPosition += step;
+        keyPosition %= slots.Length;
+      }
+
+      return -1;
+    }
+
+    private int GetMinHitSlotPosition()
+    {
+      int minHit = int.MaxValue;
+      int position = 0;
+      for (int i = 0; i < hits.Length; i++)
+      {
+        if (hits[i] < minHit)
+        {
+          minHit = hits[i];
+          position = i;
+        }
+      }
+
+      return position;
+    }
+
+    public void Put(string key, T value)
+    {
+      if (key == null)
+        return;
+
+      int position = SeekPosition(key);
+
+      if (position == -1)
+      {
+        int minHitPosition = GetMinHitSlotPosition();
+        values[minHitPosition] = default(T);
+        slots[minHitPosition] = null;
+        hits[minHitPosition] = 0;
+
+        Put(key, value);
+      }
+      else
+      {
+        if (slots[position] == null)
+          slots[position] = key;
+
+        values[position] = value;
+      }
+    }
+
+    public T Get(string key)
+    {
+      if (key == null)
+        return default(T);
+
+      int position = GetKeyPosition(key);
+
+      if (position != -1 && slots[position] != null)
+      {
+        hits[position]++;
+        return values[position];
+      }
+
+      return default(T);
+    }
+  }
+}
---
+++ NativeCacheProject/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsDataStructures
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      TestNativeCache();
+
+      Console.ReadKey();
+    }
+
+    static void TestNativeCache()
+    {
+      NativeCache<string> cache = new NativeCache<string>(10);
+
+      cache.Put("facebook", "http://facebook.com");
+      cache.Put("google", "http://google.com");
+      cache.Put("vk", "http://vk.com");
+      cache.Put("mail", "http://mail.ru");
+      cache.Put("yandex", "http://yandex.ru");
+      cache.Put("yahoo", "http://yahoo.com");
+      cache.Put("bing", "http://bing.com");
+      cache.Put("microsoft", "http://microsoft.com");
+      cache.Put("oracle", "http://oracle.com");
+
+      Console.WriteLine(new string('=', 50));
+      ShowCacheInfo(cache);
+      cache.Put("github", "http://github.com");
+      Console.WriteLine(new string('=', 50));
+      ShowCacheInfo(cache);
+
+      Console.WriteLine(new string('=', 50));
+
+      Console.WriteLine("test read from cache");
+      Console.WriteLine();
+      for (int i = 0; i < cache.slots.Length; i++)
+      {
+        for (int j = cache.slots.Length - i; j > 0; j--)
+          cache.Get(cache.slots[i]);
+      }
+
+      ShowCacheInfo(cache);
+
+      Console.WriteLine();
+      cache.Put("amazon", "http://amazon.com");
+      ShowCacheInfo(cache);
+      Console.WriteLine();
+      cache.Put("twitter", "http://twitter.com");
+      ShowCacheInfo(cache);
+      cache.Get("twitter");
+      Console.WriteLine();
+      ShowCacheInfo(cache);
+      cache.Get(null);
+      Console.WriteLine();
+      ShowCacheInfo(cache);
+    }
+
+    static void ShowCacheInfo(NativeCache<string> cache)
+    {
+      for (int i = 0; i < cache.size; i++)
+      {
+        Console.WriteLine(cache.slots[i] + " "+cache.values[i] + " " + cache.hits[i]);
+      }
+    }
+    
+  }
+}
